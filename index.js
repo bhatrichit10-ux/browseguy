19910e2dedd68bc2e9966fb6f7a18842df32b6a8
Richit Bhat <bhatrichit10@gmail.com>
2026-03-09 22:50:21 +0530

    It works on basic stuff now and supports basic navigation as of now

--- index.js
+++ index.js
@@ -2,14 +2,58 @@ import boxen from 'boxen'
 import processOut from './src/processer.js'
 import load from './src/loader.js'
 import chalk from 'chalk'
-async function main() {
-let url = 'example.com'
+import inquirer from "inquirer"
+
+
+async function main(url) {
 let html = await load(url)
 html = await processOut(html)
 const links = html.a
   .map((l, i) => `[${i + 1}] ${l.text} -> ${l.href}`)
   .join("\n")
-console.log(chalk.blue(`Links found on ${url}:` + '\n' + chalk.green(links)))
-return console.log(boxen(html.text, {padding: 1, title: chalk.red(html.title)}, {}))
+
+
+ console.log(boxen(html.text, {padding: 1, title: chalk.red(html.title)}))
+if(links.length != 0) {
+    console.log(chalk.blue(`Links found on ${url}:` + '\n' + chalk.green(links)))
+    const choices = html.a.map(link => ({
+  name: link.text,
+  value: link.href
+}))
+const { selectedLink } = await inquirer.prompt([
+  {
+    type: 'list',
+    name: 'selectedLink',
+    message: 'Select a link to visit:',
+    choices
+  }
+])
+console.log(chalk.blue(`You selected: ${selectedLink}`))
+if(selectedLink.startsWith('/')) {
+   process.stdout.write('\x1Bc')
+    main(new URL(selectedLink, url).toString())
 }
-main()
+else {
+   process.stdout.write('\x1Bc')
+    main(selectedLink)
+}
+} else {
+   process.stdout.write('\x1Bc')
+    let gugugaga = await inquirer.prompt([
+  {
+    type: 'input',
+    name: 'url',
+    message: 'Enter a URL to browse:'
+  }
+])
+    main(gugugaga.url)
+}
+}
+let gugugaga = await inquirer.prompt([
+  {
+    type: 'input',
+    name: 'url',
+    message: 'Enter a URL to browse:'
+  }
+])
+main(gugugaga.url)
