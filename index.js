9ad4916669fbd3ad99d045e60a69c5ef7ad133ac
Richit Bhat <bhatrichit10@gmail.com>
2026-03-09 20:41:43 +0530

    tester

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import processOut from './src/processer.js'
 import load from './src/loader.js'
 import chalk from 'chalk'
 async function main() {
-let url = 'debugcrew.tech'
+let url = 'example.com'
 let html = await load(url)
 html = await processOut(html)
 let atags = html.a
